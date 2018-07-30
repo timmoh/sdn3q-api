160be36dccad1d3ce6fbd1c6b6e411fdbe2b00c8
timo doersching <timo@doersching.net>
2018-07-30 17:58:12 +0200

    add model ChannelPicture

---
+++ src/Model/ChannelPicture.php
@@ -0,0 +1,21 @@
+<?php
+
+
+namespace SDN3Q\Model;
+
+class ChannelPicture extends BaseModel {
+	/**
+	 * Use Channel Picture
+	 *
+	 * @var bool
+	 * @JsonField(name="UseOnlinePicture", type="bool")
+	 */
+	public $useOnlinePicture;
+	/**
+	 * URI of Channel Picture
+	 *
+	 * @var string
+	 * @JsonField(name="OnlinePictureURI", type="string")
+	 */
+	public $onlinePictureURI;
+}
