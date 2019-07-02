e2cb7097be6d28d820ebc958b810b0c6819099d3
timo doersching <timo@doersching.net>
2019-07-02 22:33:53 +0200

    add FileFormatEnum

---
+++ src/Enum/FileFormat.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace SDN3Q\Enum;
+
+class FileFormat {
+
+	const MP4     = 'mp4';
+	const AVI     = 'avi';
+	const MOV     = 'mov';
+	const WEBM    = 'webm';
+	const THREEGP = '3gp';
+	const MP3     = 'mp3';
+	const WAV     = 'wav';
+	const AAC     = 'aac';
+
+}
