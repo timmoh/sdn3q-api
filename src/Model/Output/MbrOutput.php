99de61b921481e954235be75acc4d45d87431a82
Valentin Manthei <contact@valentin-manthei.de>
2019-08-22 22:48:32 +0200

    add mbr output property

--- src/Model/Output/MbrOutput.php
+++ src/Model/Output/MbrOutput.php
@@ -3,7 +3,14 @@
 namespace SDN3Q\Model\Output;
 
 use SDN3Q\Model\BaseModel;
+use MintWare\DMM\DataField;
 
 class MbrOutput extends BaseModel {
+
+	/**
+	 * @var string
+	 * @DataField(name="hls", type="string")
+	 */
+	public $hls;
 
 }
