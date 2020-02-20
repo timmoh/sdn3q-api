cee767af6496687c33267d7b7047f70de9ab5ad7
Valentin Manthei <valentin.manthei@swisscom.com>
2020-02-20 10:59:21 +0100

    add timeshift2vod window

---
+++ src/Model/TimeshiftWindowDimensions.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace SDN3Q\Model;
+
+use DateTime;
+use MintWare\DMM\DataField;
+
+class TimeshiftWindowDimensions extends BaseModel {
+
+	/**
+	 * @var string
+	 * @DataField(name="StartDateTime", type="string")
+	 */
+	public $startDateTime;
+
+	/**
+	 * @var string
+	 * @DataField(name="EndDateTime", type="string")
+	 */
+	public $endDateTime;
+
+}
--- src/Request/Channels/Timeshift2Vod.php
+++ src/Request/Channels/Timeshift2Vod.php
@@ -2,11 +2,35 @@
 
 namespace SDN3Q\Request\Channels;
 
+use MintWare\DMM\ObjectMapper;
+use MintWare\DMM\Serializer\JsonSerializer;
+use SDN3Q\Model\TimeshiftWindowDimensions;
 use SDN3Q\Request\BaseRequest;
 
 class Timeshift2Vod extends BaseRequest {
 
 	protected static $endpoint = 'channels';
+
+	/**
+	 * Get the Timeshift2Vod window
+	 *
+	 * @param int $channelId
+	 *
+	 * @return TimeshiftWindowDimensions
+	 * @throws \GuzzleHttp\Exception\GuzzleException
+	 */
+	public static function getTimeshift2VodWindow(int $channelId) {
+		parent::$subUrl = $channelId . '/timeshift2vod/timeshiftwindow';
+
+		try {
+			$mapper   = new ObjectMapper(new JsonSerializer());
+			$response = json_decode(self::getResponse());
+
+			return $mapper->map(json_encode($response->TimeshiftWindowDimensions), TimeshiftWindowDimensions::class);
+		} catch (\Exception $e) {
+			throw $e;
+		}
+	}
 
 	/**
 	 * Create a new Timeshift2Vod
